825cc471d2f847dfd76bbf95eca3ec8a7f1dcf5a
Reina Murooka <reinamurooka@gmail.com>
2019-07-12 19:52:16 -0400

    BFS

---
+++ Trees/BFS.js
@@ -0,0 +1,28 @@
+/*
+Breadth First Search
+
+Example input Tree:
+
+        A
+       /  \
+      B    C
+    / \   /  \
+   D   E  F   G
+    */
+
+const bfs = root => {
+  let queue = [root];
+  let BFS = [];
+
+  while (queue.length) {
+    const curr = queue.shift();
+    BFS.push(curr.value);
+    if (curr.left) {
+      queue.push(curr.left);
+    }
+    if (curr.right) {
+      queue.push(curr.right);
+    }
+  }
+  return BFS;
+};
